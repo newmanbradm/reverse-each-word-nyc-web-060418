19e0ec1dc50b63fd07846cd471a8d9ef70e566f8
Brad Newman <newmanbradm@gmail.com>
2018-05-17 20:19:22 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+def reverse_each_word(string)
+  array = string.split
+  new_array = []
+  array.each do |item|
+    new_array << item.reverse
+  end
+  new_array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split
+  new_array = array.collect {|item| item.reverse}
+  new_array.join(" ")
+end
